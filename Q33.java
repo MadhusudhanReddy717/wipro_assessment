e2430c24bf475a5a2c72e5ad8af837c308c750ba
MadhusudhanReddy717 <113969054+MadhusudhanReddy717@users.noreply.github.com>
2025-07-27 21:46:01 +0530

    Updated Q33.java

--- Q33.java
+++ Q33.java
@@ -68,3 +68,38 @@ public class Q33 {
         sc.close();
     }
 }
+/*
+Caught NegativeArraySizeException
+java.lang.NegativeArraySizeException: -5
+	at assessment1/assessment.Q32.main(Q32.java:6)
+
+Caught ArrayIndexOutOfBoundsException
+java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 3
+	at assessment1/assessment.Q32.main(Q32.java:14)
+
+Caught StringIndexOutOfBoundsException
+java.lang.StringIndexOutOfBoundsException: Index 5 out of bounds for length 3
+	at java.base/jdk.internal.util.Preconditions$1.apply(Preconditions.java:55)
+	at java.base/jdk.internal.util.Preconditions$1.apply(Preconditions.java:52)
+	at java.base/jdk.internal.util.Preconditions$4.apply(Preconditions.java:213)
+	at java.base/jdk.internal.util.Preconditions$4.apply(Preconditions.java:210)
+	at java.base/jdk.internal.util.Preconditions.outOfBounds(Preconditions.java:98)
+	at java.base/jdk.internal.util.Preconditions.outOfBoundsCheckIndex(Preconditions.java:106)
+	at java.base/jdk.internal.util.Preconditions.checkIndex(Preconditions.java:302)
+	at java.base/java.lang.String.checkIndex(String.java:4832)
+	at java.base/java.lang.StringLatin1.charAt(StringLatin1.java:46)
+	at java.base/java.lang.String.charAt(String.java:1555)
+	at assessment1/assessment.Q32.main(Q32.java:22)
+
+Caught IndexOutOfBoundsException
+java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 2
+	at assessment1/assessment.Q32.main(Q32.java:30)
+
+Caught NullPointerException
+java.lang.NullPointerException: Cannot invoke "String.length()" because "s" is null
+	at assessment1/assessment.Q32.main(Q32.java:38)
+
+Caught ArithmeticException
+java.lang.ArithmeticException: / by zero
+	at assessment1/assessment.Q32.main(Q32.java:45)
+*/
