5961ae585ce13951a473803e749276c4f398ca27
MadhusudhanReddy717 <113969054+MadhusudhanReddy717@users.noreply.github.com>
2025-08-05 09:41:00 +0530

    Add files via upload

---
+++ Q11RestController/src/main/java/com/spring/q11/Q11MainApp.java
@@ -0,0 +1,11 @@
+package com.spring.q11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q11MainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Q11MainApp.class, args);
+    }
+}
